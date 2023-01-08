25edd16faffeb9d7a78137b2cefb03584340f63f
Neuy <neuwirth.andrew9@gmail.com>
2023-01-08 16:37:47 -0500

    profile structure

--- develop/client/src/pages/Profile.js
+++ develop/client/src/pages/Profile.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_SINGLE_USER } from "../utils/queries";
-import { User } from "../../../server/models";
+// import { User } from "../../../server/models";
 
 const Profile = () => {
   const { userId } = useParams();
@@ -19,9 +19,18 @@ const Profile = () => {
     <Fragment>
       {!!data && (
         <div>
-          <h1>
-            ${User.name}'s Profile
-          </h1>
+          <div>
+            <h1>
+              {data.user.firstName} {data.user.lastName}'s Profile
+            </h1>
+            <h3>AKA: {data.user.username}</h3>
+          </div>
+          <div>
+            {data.user.post}
+          </div>
+          <div>
+            <h3>Email: {data.user.email}</h3>
+          </div>
         </div>
       )}
     </Fragment>
