fead31b34043374da876348f8129f308c2757e63
Neuy <neuwirth.andrew9@gmail.com>
2023-01-09 17:58:36 -0500

    added gif

--- develop/client/src/components/Header.js
+++ develop/client/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'bulma/css/bulma.css'
+import campfire from '../assets/images/campfire.gif'
 
   const Header = () => {
 
   return (
 
-    <div className='headerTitle'>
-      <h2>MY TRYBE</h2>
+    <div className='headerTitle is-flex'>
+      <h1>MY TRYBE</h1>
+      <img src={campfire} alt="Computer man" className='image is-64x64 mx-4'></img>
     </div>
 
   );
